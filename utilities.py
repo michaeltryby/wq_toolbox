d811dc6881ed8e34ad6ee558805f459e029c32b9
Brooke Mason <bemason@mich.edu>
2020-01-15 10:07:58 -0500

    sets purpose of python file

---
+++ utilities.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Brooke Mason
+# @Date:   2020-01-15 09:56:50
+# @Last Modified by:   Brooke Mason
+# @Last Modified time: 2020-01-15 10:07:23
+
+#like pystorms, defines threshold function and performance metric function
