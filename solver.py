281468b32f2607d0666c9e48538e6ddec98381c8
Brooke Mason <bemason@mich.edu>
2020-01-15 10:03:56 -0500

    set up solver class

---
+++ solver.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Author: Brooke Mason
+# @Date:   2020-01-15 09:57:30
+# @Last Modified by:   Brooke Mason
+# @Last Modified time: 2020-01-15 10:03:30
+
+class Solver:
+		""" builds various numpy solvers for treatment
+		input: 
+			none
+		returns:
+			?
+		"""
+	def solver1(self):
+
+	def solver2(self):
+
+	def solver3(self):
