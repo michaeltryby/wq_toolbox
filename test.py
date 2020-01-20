46d94518c307cf1940e9793cfc86a59a83b295cb
Brooke Mason <bemason@mich.edu>
2020-01-20 15:53:06 -0500

    attempting wq inside while loop before creating separate functions

--- test.py
+++ test.py
@@ -2,59 +2,46 @@
 # @Author: Brooke Mason
 # @Date:   2020-01-20 12:07:35
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-01-20 12:18:12
+# @Last Modified time: 2020-01-20 15:52:15
 
 # IMPORT 
 # Import SWMM Modules
 from pyswmm_lite import environment
-import wq_toolbox
+import numpy as np
+#import wq_toolbox
 
-# SETUP
-# Generate the state tuple for hydraulics for pyswmm_lite
-states = ["pollutantN", "pollutantL"]
-
+#SETUP
 # Build the hydraulics configuration dictionary
 config1 = {
     "swmm_input": "./test_single.inp",
-    "states": states,
-    }
-
-# Build the treatment configuration dictionary
-config2 = {
-    "pollutantN": "cstr", 1.5
-    "pollutantL": "cstr", 1.5
+    "states": [("P1", "depthN"), ("P1", "pollutantN", "1")],
     }
 
 # SIMULATION
 # Initialize the environment
 env = environment(config1, ctrl=True)
-wq = wq_toolbox(config2, env)
+#wq = wq_toolbox(config2, env)
 done = False
 state = env.initial_state()
 
 # Run Simulation
 while not done:
+	# Run water quality work
+	#Qin = environment._getNodeInflow("P1")
+	#Qout = environment._getLinkFlow("7")
+	#Cin = environment._getNodePollutant("P1")
+	#k = 0.5
+	#V = environment._getNodeVolume("P1")
 
-    new_treatment, done = wq.step()
+	# CSTR equation
+	#dCdt = Qin/V*Cin - Qout/V*Cout - k*C
 
-    """
-    All this in the step: 
-    
-    # Steps water quality simulation:
-    # 1st gets pollutant at current timestep
-    wq.Treatment.getPollutant() 
-
-    # 2nd runs treatment using specified solver
-    wq.Treatment.CSTR(solver1)
-
-    #  3rd sets new concentration at current time step
-    wq.Treatment.setPollutant()"""
+	#env._setNodePollutant()
 
     # Steps the simulation
-    new_state, done = env.step(np.ones(2))
-
+    new_state, done = env.step(np.ones(1))
     state = new_state
-    treatment = new_treatment
+
     
 # End Simulation & Close SWMM
 env.sim._model.swmm_end()
