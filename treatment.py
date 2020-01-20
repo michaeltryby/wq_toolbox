de66acaa7ab86286150af14dd3911af94d60cce0
Brooke Mason <bemason@mich.edu>
2020-01-20 15:42:42 -0500

    fleshing out CSTR method

--- treatment.py
+++ treatment.py
@@ -2,10 +2,12 @@
 # @Author: Brooke Mason
 # @Date:   2020-01-15 09:57:05
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-01-15 11:12:53
+# @Last Modified time: 2020-01-20 15:42:16
 
 # IMPORT MODULES
-from pyswmm_lite import environment
+from pyswmm_lite import environment as env
+from wq_toolbox.solver import Solve
+import math
 
 
 # MAIN CLASS FOR WQ_TOOLBOX
@@ -17,46 +19,39 @@ class Treatment:
         dictionary for pyswmm_lite with swmm_input and, action and state space `(ID, attribute)`
 	"""
 
-	def getPollutant(self, config1, log=True):
-		# get the pollutant concentration at current time step in each object
-		if i==pollutantN in config1:
-		    node_conc = environment._getNodePollutant
-		elif i==pollutantL in config1:
-		    link_conc = environment._getLinkPollutant
-		elif i==pollutantS in config1:  
-		    sub_conc = environment._getSubcatchPollutant
-
-    def setPollutant(self, config1, log=True):
-		# set the new pollutant concentration at current time step in each object
-		if i==pollutantN in config1:
-		    node_conc = environment._setNodePollutant
-		elif i==pollutantL in config1:
-		    link_conc = environment._setLinkPollutant
-		elif i==pollutantS in config1:  
-		    sub_conc = environment._setSubcatchPollutant
-    
-
     # TREATMENT OPTIONS
 	# provides various treatment options
-	def PFR(self, reaction_rate, solver):
+	def CSTR(self, reaction_rate, C_initial, solver_selected):
+		#Dynamic CSTR equation
+		Qin = env._getNodeInflow
+		Qout = env._getLinkFlow
+		Cin = env._getNodePollutant
+		k = reaction_rate
+		V = env._getNodeVolume
+
+		# CSTR equation
+		dCdt = Qin/V*Cin - Qout/V*Cout - k*C
+
+		Solve.solver_selected()
+
+		env._setNodePollutant()
+
+	"""
+	def PFR(self, solver_selected):
 		# PFR equation
 		pfr = add_equation
-
+		Solve.solver_selected()
 	
-	def CSTR(self, reaction_rate, solver):
-		# CSTR equation
-		cstr = add_equation
-
-
-	def KC_Model(self, reaction_rate, solver):
+	def KC_Model(self, solver_selected, C_star, k):
 		# KC Model equation
-		kc_model = add_equation
+
+		(C_out - C_star) = math.exp(-k/environment.get_NodeInflow)
+		Solve.solver_selected()	
 
 	# Maybe user just puts equation straight into dictionary instead?
-	def User_Defined(self, equation, solver):
+	def User_Defined(self, equation, solver_selected):
 		# Allows user to define their own treatment equation
-
-
+		Solve.solver_selected()
 
 	# METHODS TO ADD LATER
 	# log water quality data
@@ -67,5 +62,5 @@ class Treatment:
 
 	# track a pollutant through the system
 	def tracker(self):
-
+	"""
 
