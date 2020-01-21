d37dbd7645a3d857f5cb98607137e70e29a8037e
Brooke Mason <bemason@mich.edu>
2020-01-21 09:21:20 -0500

    created CSTR function to run inside while loop, currently doesn't run in loop, but function works on it's own

--- test.py
+++ test.py
@@ -2,12 +2,13 @@
 # @Author: Brooke Mason
 # @Date:   2020-01-20 12:07:35
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-01-20 16:03:53
+# @Last Modified time: 2020-01-21 09:18:42
 
 # IMPORT 
 # Import SWMM Modules
 from pyswmm_lite import environment
 import numpy as np
+from scipy.integrate import odeint
 #import wq_toolbox
 
 #SETUP
@@ -17,28 +18,36 @@ config1 = {
     "states": [("P1", "pollutantN", "1")],
     }
 
+
 # SIMULATION
 # Initialize the environment
 env = environment("./test_single.inp", ctrl=False)
-#wq = wq_toolbox(config2, env)
 done = False
+
+#SETUP WQ FUNCTION
+def CSTR(Co,t,k):
+	Qin = env._getNodeInflow("P1")
+	Qout = env._getLinkFlow("7")
+	Cin = env._getNodePollutant("P1")
+	Cout =  env._getLinkPollutant("7")
+	#V = environment._getNodeVolume("P1")
+	V = 1
+
+	# CSTR equation
+	dCdt = Qin/V*Cin - Qout/V*Cout - k*Co
+
+	#  Set new concentration
+	env._setNodePollutant("P1", dCdt)
+
+	return dCdt
 
 # Run Simulation
 while not done:
-	# Run water quality work
-	#Qin = environment._getNodeInflow("P1")
-	#Qout = environment._getLinkFlow("7")
-	#Cin = environment._getNodePollutant("P1")
-	#k = 0.5
-	#V = environment._getNodeVolume("P1")
-
-	# CSTR equation
-	#dCdt = Qin/V*Cin - Qout/V*Cout - k*C
-
-	#env._setNodePollutant()
-
-    # Steps the simulation
+	# Steps the simulation
     done = env.step()
+
+	# Run water quality work
+	odeint(CSTR, 0.0, np.linspace(0,200), k=(0.5,))
 
     
 # End Simulation & Close SWMM
