9915435b632cfcd7ad1e79be1b372a7b911954c0
Brooke Mason <bemason@mich.edu>
2020-01-21 09:00:43 -0500

    working cstr function  with  scipy  odeint solver

--- cstr.py
+++ cstr.py
@@ -2,19 +2,29 @@
 # @Author: Brooke Mason
 # @Date:   2020-01-20 15:01:08
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-01-20 15:03:01
+# @Last Modified time: 2020-01-21 08:56:15
 
-def CSTR(self, reaction_rate, C_initial, solver_selected):
-	#Dynamic CSTR equation
-	Qin = env._getNodeInflow
-	Qout = env._getLinkFlow
-	Cin = env._getNodePollutant
-	k = reaction_rate
-	V = env._getNodeVolume
+import numpy as np
+from scipy.integrate import odeint
+
+#SETUP WQ FUNCTION
+def CSTR(Co,t,k):
+	#Qin = env._getNodeInflow("P1")
+	#Qout = env._getLinkFlow("7")
+	#Cin = env._getNodePollutant("P1")
+	#V = environment._getNodeVolume("P1")
+	Qin = 50
+	Qout =  25
+	Cin = 10
+	Cout  = 5
+	V = 100
 
 	# CSTR equation
-	dCdt = Qin/V*Cin - Qout/V*Cout - k*C
+	dCdt = Qin/V*Cin - Qout/V*Cout - k*Co
+	return print(dCdt)
 
-	Solve.solver_selected()
+	#env._setNodePollutant("P1", dCdt)
 
-	env._setNodePollutant()
+k = 0.5
+t =  np.linspace(0,10,11)
+odeint(CSTR, 0.0, t, args=(k,))
