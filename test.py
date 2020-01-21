e85702d484a5619b2827e87dc201620dc44f3ef8
Brooke Mason <bemason@mich.edu>
2020-01-21 11:25:02 -0500

    updated with working cstr equation. need to finish calling it in the loop.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # @Author: Brooke Mason
 # @Date:   2020-01-20 12:07:35
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-01-21 09:18:42
+# @Last Modified time: 2020-01-21 11:16:23
 
 # IMPORT 
 # Import SWMM Modules
@@ -25,31 +25,33 @@ env = environment("./test_single.inp", ctrl=False)
 done = False
 
 #SETUP WQ FUNCTION
-def CSTR(Co,t,k):
-	Qin = env._getNodeInflow("P1")
-	Qout = env._getLinkFlow("7")
-	Cin = env._getNodePollutant("P1")
-	Cout =  env._getLinkPollutant("7")
-	#V = environment._getNodeVolume("P1")
-	V = 1
+def CSTR(C, t, k, V, Qin, Qout, Cin):
+	# Qin = env._getNodeInflow("P1", "1")
+	# Qout = env._getLinkFlow("7")
+	# Cin = env._getNodePollutant("P1")
+	# V = env.sim._model.getNodeResult("P1",3)
 
 	# CSTR equation
-	dCdt = Qin/V*Cin - Qout/V*Cout - k*Co
-
-	#  Set new concentration
+	dCdt = (Qin*Cin - Qout*C)/V - k*C
+	return dCdt
+	# Set new concentration
 	env._setNodePollutant("P1", dCdt)
 
-	return dCdt
 
 # Run Simulation
 while not done:
+	# call current time
+	env.sim.swmm_model.getCurrentSimualationTime()
 	# Steps the simulation
-    done = env.step()
+	done = env.step()
 
-	# Run water quality work
-	odeint(CSTR, 0.0, np.linspace(0,200), k=(0.5,))
+    # call current time step
+    # calculate difference between two times (det)
 
+	#Run water quality work
+	#odeint(CSTR, 0.0, np.linspace(0, 40, 100), args=(0.10, env.sim._model.getNodeResult("P1",3), env._getNodeInflow("P1"), env._getLinkFlow("7"), env._getNodePollutant("P1", "1")))
     
+
 # End Simulation & Close SWMM
 env.sim._model.swmm_end()
 env.sim._model.swmm_close()
