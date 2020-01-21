a8270344ace38eb43ecd89e7e526ca0e892c68b3
Brooke Mason <bemason@mich.edu>
2020-01-21 11:25:47 -0500

    updated

--- solver.py
+++ solver.py
@@ -2,7 +2,7 @@
 # @Author: Brooke Mason
 # @Date:   2020-01-15 09:57:30
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-01-15 10:24:42
+# @Last Modified time: 2020-01-20 13:53:39
 
 class Solve:
 		""" builds various numpy solvers for treatment
