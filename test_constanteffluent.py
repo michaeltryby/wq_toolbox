7624518deae3b36a083d55082c1bb3ebe0d7b32e
Brooke Mason <bemason@mich.edu>
2020-02-13 12:17:47 -0500

    modified code with Abhi to test the getters/setters

--- test_constanteffluent.py
+++ test_constanteffluent.py
@@ -2,7 +2,7 @@
 # @Author: Brooke Mason
 # @Date:   2020-02-05 10:05:44
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-02-11 16:02:58
+# @Last Modified time: 2020-02-13 11:30:32
 
 # IMPORT
 # Import modules
@@ -15,39 +15,28 @@ import matplotlib.pyplot as plt
 env = environment("./tank_test_forcomparison.inp", ctrl=False)
 done = False
 conc = []
-#depth = []
-#flow = []
+conc2 = []
+depth = []
+flow = []
 
+env._setNodePollutant("Tank", 0, 5)
+
+inflow = np.zeros(50000)
+inflow[0:5000] = 5.0
+ggg = 1
 # Run Simulation
 while not done:
+    env.sim._model.setNodeInflow("Tank", inflow[ggg])
+    env._setNodePollutant("Tank", 0, 5)
+
     # Steps th esimulation
     done = env.step()
-    """
-    # Set new concentration (try 1)
-    c = env._getNodePollutant("Tank", 0)
-    if 0 <= c <= 5: 
-        pass
-    elif c > 5:
-        env._setNodePollutant("Tank", 0, 10)
 
-
-
-    # Set new concentration (try 2)
-    c = env._getNodePollutant("Tank", 0)
-    if 0 <= c <= 5: 
-        pass
-    elif c > 5:
-        d = min(c, 5)
-        env._setNodePollutant("Tank", 0, d)
-        """
-
-
-    #env._getNodePollutant("Tank", 0)
-    #env._setNodePollutant("Tank", 0, 5)
+    ggg += 1
     conc.append(env._getNodePollutant("Tank", 0))
-    #conc2.append(env._getLinkPollutant("Valve", 0))
-    #depth.append(env._getNodeDepth("Tank"))
-    #flow.append(env._getNodeInflow("Outfall"))
+    conc2.append(env._getLinkPollutant("Valve", 0))
+    depth.append(env._getNodeDepth("Tank"))
+    flow.append(env._getNodeInflow("Tank"))
 
 
 # End Simulation & Close SWMM
@@ -57,8 +46,14 @@ env.sim._model.swmm_close()
 """
 TO DO: Create a class for graphing treatment results
 """
+plt.subplot(1,4,1)
 plt.plot(conc)
-#plt.plot(conc2)
+plt.subplot(1,4,2)
+plt.plot(conc2)
+plt.subplot(1,4,3)
+plt.plot(depth)
+plt.subplot(1,4,4)
+plt.plot(flow)
 #plt.xlabel("Time (s)")
 plt.ylabel("Concentration")
 plt.show()
