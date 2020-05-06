e555097bc4e48054ef2db43304d0c9052a479030
Brooke Mason <bemason@mich.edu>
2020-05-06 08:24:47 -0400

    building package

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name="wq_toolbox",
     version="0.0.1",
     description="PySWMM toolbox for modelling any pollutant generation or removal method",
     author="Brooke Mason, Abhiram Mullapudi",
     author_email="bemason@umich.edu, abhiramm@umich.edu",
     long_description=long_description,
-    url="https://github.com/kLabUM/wq_toolbox",
+    long_description_content_type="text/markdown",
+    url="https://github.com/bemason/wq_toolbox",
     packages=['wq_toolbox'],
-    package_data=setuptools.find_packages(),
     classifiers=[
         "Programming Lanageuage :: Python :: 3",
         "License :: OSI Apprpoved :: GNU Lesser General Public License v3 (LGPLv3)",
