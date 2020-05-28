82186f0b862f71f10e8a533722aebb3d10bbed31
Brooke Mason <bemason@mich.edu>
2020-05-28 14:59:15 -0400

    code ready for simulations

--- toolbox_NO.py
+++ toolbox_NO.py
@@ -2,10 +2,10 @@
 # @Author: Brooke Mason
 # @Date:   2020-01-15 09:57:05
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-05-26 10:41:30
+# @Last Modified time: 2020-05-28 13:33:14
 
 # Import required modules
-from pyswmm import Simulation, Nodes
+from pyswmm import Simulation, Nodes, Links
 from wq_toolbox.nodes import Node_Quality
 from scipy.integrate import ode 
 import numpy as np
@@ -16,34 +16,40 @@ def CSTR_tank(t, C, Qin, Cin, Qout, V, k):
     dCdt = (Qin*Cin - Qout*C)/V + k*C
     return dCdt
 
-# Make dictionaries for each water quality method
-# From Reddy et al. 1980
-dict4 = {'Tank2': {0: [0.0003, 10]}}
-dict5 = {'Tank2': {0: [0.0003, 10]}}
-dict6 = {'Tank2': {0: [0.0003, 10]}}
-
 # Lists to store results
-tank2_flow = []
-tank2_vol = []
-tank2_depth = []
-tank2_N = []
-tank2_DO1 = []
-tank2_DO2 = []
-tank2_DO3 = []
+Wetland_conc = []
+Wetland_flow = []
+Wetland_vol = []
+Wetland_depth = []
+Wetland_N = []
+Wetland_DO1 = []
+Wetland_DO2 = []
+Wetland_DO3 = []
 Qin = []
 Qout = []
 Cin = []
 V = []
 
+# DO Variables
+k_DO = 0.000015
+Co_DO1 = 10.0
+Co_DO2 = 5.5
+Co_DO3 = 3.0
+t = 0
+
+# Tank Inflows
+Qin1 = np.genfromtxt('Qin1.txt', delimiter=',')
+Qin2 = np.genfromtxt('Qin2.txt', delimiter=',')
+Qin3 = np.genfromtxt('Qin3.txt', delimiter=',')
+
 # Setup toolbox simulation
-with Simulation("./tanks_channel_NO.inp") as sim:
-    # Setup toolbox methods
-    #DO_Layer1 = Node_Quality(sim, dict4)
-    #DO_Layer2 = Node_Quality(sim, dict5)
-    #DO_Layer3 = Node_Quality(sim, dict6)
-    
+with Simulation("./Ellsworth_Doyle_NO.inp") as sim:
     # Get asset information
-    Tank2 = Nodes(sim)["Tank2"]
+    Ellsworth = Nodes(sim)["93-50408"]
+    Doyle_Basin = Nodes(sim)["93-50404"]
+    Channel = Links(sim)["95-69044"]
+    Dwn_Channel = Nodes(sim)["97-50264"]
+    Wetland = Nodes(sim)["93-49759"]
     
     # Setup dt calculation        
     start_time = sim.start_time
@@ -59,74 +65,82 @@ with Simulation("./tanks_channel_NO.inp") as sim:
 
     # Step through the simulation    
     for index,step in enumerate(sim):
+                
+        # Set inflow
+        sim._model.setNodeInflow("93-50408", Qin1[index])
+        sim._model.setNodeInflow("93-50404", Qin2[index])
+        sim._model.setNodeInflow("93-49759", Qin3[index])
+
         # Calculate dt
         current_step = sim.current_time
         dt = (current_step - last_timestep).total_seconds()
         last_timestep = current_step
+        t += dt
 
         # Calculate DO concentration in tank layers
         # Get depth to calculate DO
-        """
-        depth = sim._model.getNodeResult("Tank2", 5)
-        tank2_depth.append(depth)
-        
-        if depth <= 1.66:
+        depth = sim._model.getNodeResult("93-49759", 5)
+        Wetland_depth.append(depth)
+
+        # reset DO if tank is empty
+        if depth <= 0.01:
+            t = 0
+
+        if depth <= 3.00:
             # Calculate DO concentration in first layer
-            DO1 = DO_Layer1.DO_solver(index)
+            DO1 = Co_DO1*np.exp(-k_DO*t)
             # Calculate nitate reaction rate based on DO concentration
             if DO1 < 1.0:
-                k_ni = 0.00014  # [1/s]
+                k_ni = 0.000006  # [1/s]
             else:
                 k_ni = 0.0
-            tank2_DO1.append(DO1)
+            Wetland_DO1.append(DO1)
         
-        elif 1.66 < depth <= 3.33:
+        elif 3.0 < depth <= 6.0:
             # Calculate DO concentration in first two layers
-            DO1 = DO_Layer1.DO_solver(index)
-            DO2 = DO_Layer2.DO_solver(index)
+            DO1 = Co_DO1*np.exp(-k_DO*t)
+            DO2 = Co_DO2*np.exp(-k_DO*t)
             # Calculate nitate reaction rate based on DO concentration
             if DO1 < 1.0:
-                k_ni1 = 0.00014
+                k_ni1 = 0.000006
             else:
                 k_ni1 = 0.0
             if DO2 < 1.0:
-                k_ni2 = 0.00014
+                k_ni2 = 0.000006
             else:
                 k_ni2 = 0.0
             k_ni = k_ni1 + k_ni2
-            tank2_DO1.append(DO1)
-            tank2_DO2.append(DO2)
+            Wetland_DO1.append(DO1)
+            Wetland_DO2.append(DO2)
         else:
             # Calculate DO concentration in all three layers
-            DO1 = DO_Layer1.DO_solver(index)
-            DO2 = DO_Layer2.DO_solver(index)
-            DO3 = DO_Layer3.DO_solver(index)
+            DO1 = Co_DO1*np.exp(-k_DO*t)
+            DO2 = Co_DO2*np.exp(-k_DO*t)
+            DO3 = Co_DO3*np.exp(-k_DO*t)
             # Calculate nitate reaction rate based on DO concentration
             if DO1 < 1.0:
-                k_ni1 = 0.00014
+                k_ni1 = 0.000006
             else:
                 k_ni1 = 0.0
             if DO2 < 1.0:
-                k_ni2 = 0.00014
+                k_ni2 = 0.000006
             else:
                 k_ni2 = 0.0
             if DO3 < 1.0:
-                k_ni3 = 0.00014
+                k_ni3 = 0.000006
             else:
-                k_nil3 = 0.0
+                k_ni3 = 0.0
             k_ni = k_ni1 + k_ni2 + k_ni3
-            tank2_DO1.append(DO1)
-            tank2_DO2.append(DO2)
-            tank2_DO3.append(DO3)
-        """
-        # Calculate NO concnetration in tanks
-        # Get parameters to calculate NO
-        Qin=sim._model.getNodeResult("Tank2",0)
-        Cin=sim._model.getNodeCin("Tank2",0)
-        Qout=sim._model.getNodeResult("Tank2",1)
-        V=sim._model.getNodeResult("Tank2",3)
-        k_ni = 0.00014
+            Wetland_DO1.append(DO1)
+            Wetland_DO2.append(DO2)
+            Wetland_DO3.append(DO3)
         
+        # Calculate NO concentration in tanks
+        # Get parameters to calculate NO
+        Qin=sim._model.getNodeResult("93-49759",0)
+        Cin=sim._model.getNodeCin("93-49759",0)
+        Qout=sim._model.getNodeResult("93-49759",1)
+        V=sim._model.getNodeResult("93-49759",3)
 
         #Solve ODE
         if index == 0:
@@ -151,53 +165,66 @@ with Simulation("./tanks_channel_NO.inp") as sim:
             solver3.integrate(solver3.t+dt)
         
         # Set new concentration
-        sim._model.setNodePollutant("Tank2", 0, solver3.y[0])
+        sim._model.setNodePollutant("93-49759", 0, solver3.y[0])
 
         # Get Nitrate concentration        
-        c3_N = Tank2.pollut_quality
-        tank2_N.append(c3_N['NO'])
+        c3_N = Wetland.pollut_quality
+        Wetland_N.append(c3_N['NO'])
 
         # Get flow for each asset
-        tank2_flow.append(sim._model.getNodeResult("Tank2", 0))
-        tank2_vol.append(sim._model.getNodeResult("Tank2", 3))
+        Wetland_flow.append(sim._model.getNodeResult("93-49759", 0))
+        Wetland_vol.append(sim._model.getNodeResult("93-49759", 3))
 
-
-        
 #----------------------------------------------------------------------#
 # Confirm CSTR matches steady state equilibrium concentration
 # Found average V and Q for simulation, r same as k above 
-V = sum(tank2_vol)/len(tank2_vol)
-Q = sum(tank2_flow)/len(tank2_flow)
-k = 0.00014
+"""
+V = sum(Wetland_vol)/len(Wetland_vol)
+Q = sum(Wetland_flow)/len(Wetland_flow)
+k = 0.000014
 Co = 10
-Cn = Co /((1+(k*V/Q))**3)
-print(Cn)
-
+Cn = Co /((1+(k*V/Q))**2)
+print("Cn", Cn)
+"""
 #----------------------------------------------------------------------#
 # Plot Results
 
 # Calculate load
-Nload3 = [a*b for a,b in zip(tank2_N,tank2_flow)]
-Ncum_load3 = np.cumsum(Nload3)
+Nload = [a*b for a,b in zip(Wetland_N,Wetland_flow)]
+Ncum_load = np.cumsum(Nload)
 
 # SS graphs
-SS = [9.88]*len(Ncum_load3)
+#SS = [Cn]*len(Ncum_load3)
 
-plt.subplot(311)
-plt.plot(tank2_N, 'm', label='Tank2')
-plt.plot(SS, 'k', label='SteadyState')
+plt.subplot(511)
+plt.plot(Wetland_N, 'm', label='Wetland')
+#plt.plot(SS, 'k', label='SteadyState')
 plt.ylabel("NO Conc")
 plt.xlabel("Time (s)")
 plt.legend()
 
-plt.subplot(312)
-plt.plot(tank2_flow, 'm', label='Tank2')
+plt.subplot(512)
+plt.plot(Wetland_DO1, 'b:', label='DO_Layer1')
+plt.plot(Wetland_DO2, 'r:', label='DO_Layer2')
+plt.plot(Wetland_DO3, 'g:', label='DO_Layer3')
+plt.ylabel("DO Conc")
+plt.xlabel("Time (s)")
+plt.legend()
+
+plt.subplot(513)
+plt.plot(Wetland_depth, 'm', label='Wetland')
+plt.ylabel("Depth")
+plt.xlabel("Time (s)")
+plt.legend()
+
+plt.subplot(514)
+plt.plot(Wetland_flow, 'm', label='Wetland')
 plt.ylabel("Flow")
 plt.xlabel("Time (s)")
 plt.legend()
 
-plt.subplot(313)
-plt.plot(Ncum_load3, 'm:', label='Tank2')
+plt.subplot(515)
+plt.plot(Ncum_load, 'm', label='Wetland')
 plt.ylabel("NO Cumm Load")
 plt.xlabel("Time (s)")
 plt.legend()
--- toolbox_TSS.py
+++ toolbox_TSS.py
@@ -2,7 +2,7 @@
 # @Author: Brooke Mason
 # @Date:   2020-01-15 09:57:05
 # @Last Modified by:   Brooke Mason
-# @Last Modified time: 2020-05-19 11:48:27
+# @Last Modified time: 2020-05-28 13:00:56
 
 # Import required modules
 from pyswmm import Simulation, Nodes, Links
@@ -12,116 +12,134 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Make dictionaries for each water quality method
-# C_s from gravity Settling example in SWMM manual, v_s from Troutman et al. 2020
-dict1 = {'Tank1': {0: [0.01, 20]}, 'Tank2': {0: [0.01, 20]}}
-# From ?? 
-dict2 = {'Channel': {0: [10.0, 0.0001, 2.68, 0.68]}}
-
+# C_s amd v_s from gravity Settling example in SWMM manual
+dict1 = {'93-50408': {0: [0.01, 20]}, '93-50404': {0: [0.01, 20]}} 
+dict2 = {'95-69044': {0: [68.0, 0.0037, 1.28, 0.035]}}
 
 # Lists to store results
-ch1 = []
-tank1 = []
-tank2 = []
-ch1_flow =[]
-tank1_flow = []
-tank2_flow = []
-ch1_N = []
-tank1_N = []
-tank2_N = []
+Channel_conc = []
+Channel_flow =[]
+Ellsworth_conc = []
+Ellsworth_flow = []
+Dwn_Channel_conc = []
+Dwn_Channel_flow = []
+Doyle_Basin_conc = []
+Doyle_Basin_flow = []
+
+# Tank Inflows
+Qin1 = np.genfromtxt('Qin1.txt', delimiter=',')
+Qin2 = np.genfromtxt('Qin2.txt', delimiter=',')
+Qin3 = np.genfromtxt('Qin3.txt', delimiter=',')
 
 # Setup toolbox simulation
-with Simulation("./tanks_channel_TSS.inp") as sim:
+with Simulation("./Ellsworth_Doyle_TSS.inp") as sim:
     # Setup toolbox methods
     GS = Node_Quality(sim, dict1)
     ER = Link_Quality(sim, dict2)
     # Get asset information
-    Tank1 = Nodes(sim)["Tank1"]
-    Tank2 = Nodes(sim)["Tank2"]
-    Ch1 = Links(sim)["Channel"]
+    Ellsworth = Nodes(sim)["93-50408"]
+    Doyle_Basin = Nodes(sim)["93-50404"]
+    Channel = Links(sim)["95-69044"]
+    Dwn_Channel = Nodes(sim)["97-50264"]
+    Wetland = Nodes(sim)["93-49759"]
 
     # Step through the simulation    
     for index,step in enumerate(sim):
+        
+        # Set inflow
+        sim._model.setNodeInflow("93-50408", Qin1[index])
+        sim._model.setNodeInflow("93-50404", Qin2[index])
+        sim._model.setNodeInflow("93-49759", Qin3[index])
+
         # Calculate gravity settling 
         GS.GravitySettling()
         # Calculate erosion produced
         ER.Erosion()
 
         # Get TSS conc for each asset        
-        c1 = Ch1.pollut_quality
-        ch1.append(c1['TSS'])
-        c2 = Tank1.pollut_quality
-        tank1.append(c2['TSS'])
-        c3 = Tank2.pollut_quality
-        tank2.append(c3['TSS'])
+        c1 = Channel.pollut_quality
+        Channel_conc.append(c1['TSS'])
+        c2 = Ellsworth.pollut_quality
+        Ellsworth_conc.append(c2['TSS'])
+        c3 = Dwn_Channel.pollut_quality
+        Dwn_Channel_conc.append(c3['TSS'])
+        c4 = Doyle_Basin.pollut_quality
+        Doyle_Basin_conc.append(c4['TSS'])
 
         # Get flow for  each asset
-        ch1_flow.append(sim._model.getLinkResult("Channel", 0))
-        tank1_flow.append(sim._model.getNodeResult("Tank1", 0))
-        tank2_flow.append(sim._model.getNodeResult("Tank2", 0))
-
+        Channel_flow.append(sim._model.getLinkResult("95-69044", 0))
+        Ellsworth_flow.append(sim._model.getNodeResult("93-50408", 0))
+        Dwn_Channel_flow.append(sim._model.getNodeResult("97-50264", 0))
+        Doyle_Basin_flow.append(sim._model.getNodeResult("93-50404", 0))
+        
        
 #----------------------------------------------------------------------#
 # Confirm gravity settling matches toolbox simulation
 swmm_c = []
 swmm_f = []
 
-with Simulation("./tanks_channel_TSS_SWMM.inp") as sim:
-    Tank1 = Nodes(sim)["Tank1"]
+with Simulation("./Ellsworth_Doyle_TSS_SWMM.inp") as sim:
+    Ellsworth = Nodes(sim)["93-50408"]
+    Doyle_Basin = Nodes(sim)["93-50404"]
+    Wetland = Nodes(sim)["93-49759"]
     # Step through the simulation    
     for step in sim:
+        # Set inflow
+        sim._model.setNodeInflow("93-50408", Qin1[index])
+        sim._model.setNodeInflow("93-50404", Qin2[index])
+        sim._model.setNodeInflow("93-49759", Qin3[index])
         # Get newQual for Tank
-        co = Tank1.pollut_quality
+        co = Ellsworth.pollut_quality
         swmm_c.append(co['TSS'])
         # Get flow for Tank
-        swmm_f.append(sim._model.getNodeResult("Tank1", 0))
+        swmm_f.append(sim._model.getNodeResult("93-50408", 0))
 
 #----------------------------------------------------------------------#
-# Confirm erosion mass balance
 # Calculate load each timestep
-load1 = [a*b for a,b in zip(ch1,ch1_flow)]
-load2 = [a*b for a,b in zip(tank2,tank2_flow)]
+load1 = [a*b for a,b in zip(Channel_conc,Channel_flow)]
+load2 = [a*b for a,b in zip(Ellsworth_conc,Ellsworth_flow)]
+load3 = [a*b for a,b in zip(Dwn_Channel_conc,Dwn_Channel_flow)]
+load4 = [a*b for a,b in zip(swmm_c,swmm_f)]
+load5 = [a*b for a,b in zip(Doyle_Basin_conc,Doyle_Basin_flow)]
 
 # Calculate cumulative load
 cum_load1 = np.cumsum(load1)
 cum_load2 = np.cumsum(load2)
+cum_load3 = np.cumsum(load3)
+cum_load4 = np.cumsum(load4)
+cum_load5 = np.cumsum(load5)
 
+
+# Confirm erosion mass balance in Channel
 # Calculate error
-error = (cum_load1[-1]/cum_load2[-1])/cum_load1[-1]
-print(error)
+#error = (cum_load1[-1]/cum_load3[-1])/cum_load1[-1]
+#print(error)
 
 #----------------------------------------------------------------------#
 # Plot Results
-
-# Calculate load
-load3 = [a*b for a,b in zip(tank2,tank2_flow)]
-cum_load3 = np.cumsum(load3)
-
-load4 = [a*b for a,b in zip(swmm_c,swmm_f)]
-cum_load4 = np.cumsum(load4)
-
 plt.subplot(311)
-plt.plot(ch1, 'g--', label='Channel')
-plt.plot(tank1, 'b:', label='Tank1')
-plt.plot(tank2, 'm:', label='Tank2')
-plt.plot(swmm_c, 'r', label="SWMM_Tank1")
+plt.plot(Channel_conc, 'g--', label='Channel')
+plt.plot(Ellsworth_conc, 'b--', label='Ellsworth')
+plt.plot(Doyle_Basin_conc, 'm--', label='Doyle Basin')
+#plt.plot(swmm_c, 'r--', label="SWMM_Ellsworth")
 plt.ylabel("TSS Conc")
 plt.xlabel("Time (s)")
 plt.legend()
 
 plt.subplot(312)
-plt.plot(ch1_flow, 'g--', label='Channel')
-plt.plot(tank1_flow, 'b:', label='Tank1')
-plt.plot(tank2_flow, 'm:', label='Tank2')
-plt.plot(swmm_f, 'r', label="SWMM_Tank1")
+plt.plot(Channel_flow, 'g--', label='Channel')
+plt.plot(Ellsworth_flow, 'b--', label='Ellsworth')
+plt.plot(Doyle_Basin_flow, 'm--', label='Doyle Basin')
+#plt.plot(swmm_f, 'r--', label="SWMM_Ellsworth")
 plt.ylabel("Flow")
 plt.xlabel("Time (s)")
 plt.legend()
 
 plt.subplot(313)
 plt.plot(cum_load1, 'g--', label='Channel')
-plt.plot(cum_load2, 'b:', label='Tank1')
-plt.plot(cum_load3, 'm:', label='Tank2')
-plt.plot(cum_load4, 'r', label="SWMM_Tank1")
+plt.plot(cum_load2, 'b--', label='Ellsworth')
+plt.plot(cum_load5, 'm--', label='Doyle Basin')
+#plt.plot(cum_load4, 'r--', label="SWMM_Ellsworth")
 plt.ylabel("TSS Cumm Load")
 plt.xlabel("Time (s)")
 plt.legend()
